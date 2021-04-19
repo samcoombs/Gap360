b1c62d22cf41c57e7696060821449460ad4b909e
samcoombs <76431238+samcoombs@users.noreply.github.com>
2021-04-19 17:15:04 +0100

    Fixed Scroll
    The mobile nav menu now prevents scrolling when open and allows it when closed.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 const $hamburger = $(".hamburger");
 const $navList = $(".nav__list");
+const $html = $("html");
+let isOpen = false;
 
 $hamburger.on("click", function () {
-  $navList.slideToggle().css("display", "flex");
+  if (!isOpen) {
+    $navList.slideDown().css("display", "flex");
+    $html.css("overflow-y", "hidden");
+    isOpen = true;
+  } else {
+    $navList.slideUp();
+    isOpen = false;
+  }
 });
